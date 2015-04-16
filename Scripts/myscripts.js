1e7c1970f6064d850fb95e80984f90c61904cca8
bdubaya <waters.bennie@gmail.com>
2015-04-16 10:05:57 -0400

    g

--- Scripts/myscripts.js
+++ Scripts/myscripts.js
@@ -3,7 +3,25 @@ var main = function(){
 		smoothHeight: true,
 		slideshowSpeed: 5000
 	});
-	
+	$(function() {
+		$( "#dialog" ).dialog({
+		  autoOpen: false,
+		  show: {
+			effect: "blind",
+			duration: 500
+		  },
+		  hide: {
+			effect: "explode",
+			duration: 500
+		  }
+		});
+ 
+		
+	});
+	var div = document.getElementsByClassName('card')[0];
+		div.addEventListener('click', function (event) {
+		  $( "#dialog" ).dialog( "open" );
+		});
 };
 
 $(window).load(main);
