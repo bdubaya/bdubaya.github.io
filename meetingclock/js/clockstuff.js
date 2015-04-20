a0565bb64c6d289d0a9636a9d783dcad987bb8e3
bdubaya <waters.bennie@gmail.com>
2015-04-20 10:41:33 -0400

    Fixed procrastitron
    yay it works for multiple people

--- meetingclock/js/clockstuff.js
+++ meetingclock/js/clockstuff.js
@@ -40,6 +40,7 @@ $(document).ready(function(){
 	$('#reset').click(function(){
 		myWatch.reset();
 		myWatch.onTick();
+		$('#moneyDisplay').text("0.00");
 	});
 	var prevTime = 0;
 	setInterval(getMoney, 1000);
@@ -52,30 +53,18 @@ function updatedisplay(watch) {
 function getMoney(){ //used to have prevTime as an argument, but then I'm a FUCKING CLOD
 	if (watchOn){
 		var table = $('#wasters');
-		/*
-		var currTime = myWatch.getElapsed();
-		var time = currTime - prevTime; //fuck, this is always going to be 1000. BENNIE YOU IDIOT
-		*/
+		
 		var interval = 1000;
 		var currMoney = 0;
 		var amt = 0;
 		
-		/*for (var i = 0; i < table.rows.length; i++){
-			cellVal = table.rows[i][1].innerHTML;
-			//value += cellVal *
-			amt = cellVal / 52 / 40 / 60 / 60;
-		}*/
+		
 		$('.sal').each(function(){
-			cellVal = $(this).value;
-			console.log(cellVal);
+			cellVal = parseInt($(this).text());
 			amt += cellVal / 52 / 40 / 60 / 60;
-			
 		});
-		amt = amt.toFixed(2);
-		//$('#moneyDisplay').value += amt;
-		//document.getElementById('moneyDisplay').value = amt;
-		var noombro = parseInt($('#moneyDisplay').text()) + amt;
-		//alert(amt);
-		document.getElementById('moneyDisplay').innerHTML = noombro;
+		var noombro = parseFloat($('#moneyDisplay').text()) + amt;
+		console.log(noombro);
+		document.getElementById('moneyDisplay').innerHTML = noombro.toFixed(2);
 	}
 }
